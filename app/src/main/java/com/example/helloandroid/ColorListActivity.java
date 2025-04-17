7ecdd018107633f6c3aca82bc93d89be44390845
Luigi Mendoza <ludk17@gmail.com>
2025-04-17 09:27:05 -0500

    add list colors

--- app/src/main/java/com/example/helloandroid/ColorListActivity.java
+++ app/src/main/java/com/example/helloandroid/ColorListActivity.java
@@ -1,8 +1,11 @@
 package com.example.helloandroid;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -14,6 +17,7 @@ import com.example.helloandroid.adpaters.ColorAdapter;
 import com.example.helloandroid.entities.Color;
 import com.example.helloandroid.entities.ColorResponse;
 import com.example.helloandroid.services.ColorService;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,6 +29,14 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ColorListActivity extends AppCompatActivity {
+
+    RecyclerView rvColors;
+    boolean isLoading = false;
+    boolean isLastPage = false;
+    int currentPage = 1;
+
+    List<Color> data = new ArrayList<>();
+    ColorAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,42 +50,97 @@ public class ColorListActivity extends AppCompatActivity {
         });
 
 
+        Toast.makeText(getApplicationContext(), "ColorListActivity onCreate", Toast.LENGTH_SHORT).show();
 
-//        data.add(new Color("Red", "#F44336"));
-//        data.add(new Color("Pink", "#E91E63"));
-//        data.add(new Color("Blue", "#2196F3"));
-//        data.add(new Color("LightSalmon", "#FFA07A"));
-
-        RecyclerView rvColors = findViewById(R.id.rvListColors);
-        rvColors.setLayoutManager(new LinearLayoutManager(this));
-
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://www.csscolorsapi.com")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-
-        ColorService service = retrofit.create(ColorService.class);
-
-        service.getColors().enqueue(new Callback<ColorResponse>() {
-            @Override
-            public void onResponse(Call<ColorResponse> call, Response<ColorResponse> response) {
-
-                if (!response.isSuccessful()) return;
-
-                List<Color> data = response.body().colors;
-
-                ColorAdapter adapter = new ColorAdapter(data);
-                rvColors.setAdapter(adapter);
-            }
-
-            @Override
-            public void onFailure(Call<ColorResponse> call, Throwable throwable) {
-
-            }
+        FloatingActionButton button = findViewById(R.id.fabGoToColorForm);
+        button.setOnClickListener(v -> {
+            Intent intent = new Intent(this, FormColorActivity.class);
+            startActivity(intent);
         });
 
+        rvColors = findViewById(R.id.rvListColors);
+        rvColors.setLayoutManager(new LinearLayoutManager(this));
+
+        setUpRecyclerView();
+
+        loadMoreColors();
+
+    }
 
 
+    @Override
+    protected  void onResume() {
+        super.onResume();
 
+        Toast.makeText(getApplicationContext(), "ColorListActivity onResume", Toast.LENGTH_SHORT).show();
+
+    }
+
+    private void loadMoreColors() {
+
+        isLoading = true;
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://67ff051e58f18d7209efd099.mockapi.io")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        ColorService service = retrofit.create(ColorService.class);
+        service.getColors(20, currentPage).enqueue(new Callback<List<Color>>() {
+            @Override
+            public void onResponse(Call<List<Color>> call, Response<List<Color>> response) {
+                isLoading = false;
+
+                if (!response.isSuccessful()) return;
+                if (response.body() == null) return;
+                if (response.body().size() == 0) {
+                    isLastPage = true;
+                    return;
+                }
+
+
+                data.addAll(response.body());
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onFailure(Call<List<Color>> call, Throwable throwable) {
+                isLoading = false;
+            }
+        });
+    }
+
+
+    private void setUpRecyclerView() {
+
+        adapter = new ColorAdapter(data);
+        rvColors.setAdapter(adapter);
+
+        rvColors.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+            }
+
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+
+                LinearLayoutManager layoutManager = (LinearLayoutManager) recyclerView.getLayoutManager();
+                if (layoutManager == null) return;
+
+                int visibleItemCount = layoutManager.getChildCount();
+                int totalItemCount = layoutManager.getItemCount();
+                int firstVisibleItemPosition = layoutManager.findFirstVisibleItemPosition();
+
+                if (!isLoading && !isLastPage) {
+                    if ((visibleItemCount + firstVisibleItemPosition) >= totalItemCount
+                            && firstVisibleItemPosition >= 0) {
+
+                        currentPage++;
+                        loadMoreColors();
+                    }
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/helloandroid/adpaters/ColorAdapter.java
+++ app/src/main/java/com/example/helloandroid/adpaters/ColorAdapter.java
@@ -1,14 +1,17 @@
 package com.example.helloandroid.adpaters;
 
+import android.content.Intent;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.helloandroid.FormColorActivity;
 import com.example.helloandroid.R;
 import com.example.helloandroid.entities.Color;
 
@@ -35,14 +38,29 @@ public class ColorAdapter extends RecyclerView.Adapter<ColorAdapter.ColorViewHol
 
         Color color = data.get(position);
 
+
+
         TextView tvColorName = holder.itemView.findViewById(R.id.tvColorName);
         TextView tvColorHex = holder.itemView.findViewById(R.id.tvColorHex);
         View vColorBg = holder.itemView.findViewById(R.id.vColorBg);
 
         tvColorName.setText(color.nombre);
 
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(), "Color: " + color.nombre, Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(v.getContext(), FormColorActivity.class);
+                intent.putExtra("colorId", color.id);
+                intent.putExtra("colorName", color.nombre);
+                intent.putExtra("colorHex", color.colorHex);
+
+                v.getContext().startActivity(intent);
+            }
+        });
+
         try {
-            String hex = "#" + color.colorHex;
+            String hex = color.colorHex;
             tvColorHex.setText(hex);
             vColorBg.setBackgroundColor(android.graphics.Color.parseColor(hex));
         } catch(Exception ex) {
